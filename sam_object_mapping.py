dc2c06ae4743f55fad3bf870013cfb01b0388f87
RuiyuanLyu <ruiyuanlyu@gmail.com>
2024-01-12 15:17:16 +0800

    bug fix. Now the coordinate transformation is correct.

--- sam_object_mapping.py
+++ sam_object_mapping.py
@@ -3,7 +3,7 @@ import numpy as np
 import json
 import open3d as o3d
 
-def map_sam_id_to_bboxes(sam_img_path, depth_img_path, sam_json_path, intrinsic_path, depth_intrinsic_path, extrinsic_path, bbox_json_path, return_sam_id_to_bbox_idx=False):
+def map_sam_id_to_bboxes(sam_img_path, depth_img_path, sam_json_path, intrinsic_path, depth_intrinsic_path, extrinsic_path, extra_extrinsic_path, bbox_json_path, return_sam_id_to_bbox_idx=False):
     """
         Derives SAM point cloud, then matche SAM ids with bboxes, and returns the idx mapping
         Args:
@@ -21,7 +21,7 @@ def map_sam_id_to_bboxes(sam_img_path, depth_img_path, sam_json_path, intrinsic_
     """
     sam_id_to_bbox_idx = dict()
     sam_id_to_object_id = dict()
-    group_of_sam_pcds, sam_id_to_pcd_idx = generate_sam_pointcloud(sam_img_path, depth_img_path, sam_json_path, intrinsic_path, depth_intrinsic_path, extrinsic_path, return_idx_mapping=True)
+    group_of_sam_pcds, sam_id_to_pcd_idx = generate_sam_pointcloud(sam_img_path, depth_img_path, sam_json_path, intrinsic_path, depth_intrinsic_path, extrinsic_path, extra_extrinsic_path, return_idx_mapping=True)
     bboxes, object_ids, object_types = read_bboxes_json(bbox_json_path, return_id=True, return_type=True)
     sam_pcd_to_bbox_idx = match_grouped_points_with_bboxes(group_of_sam_pcds, bboxes)
     for sam_id, pcd_idx in sam_id_to_pcd_idx.items():
@@ -37,7 +37,7 @@ def map_sam_id_to_bboxes(sam_img_path, depth_img_path, sam_json_path, intrinsic_
     return sam_id_to_object_id
 
 
-def generate_sam_pointcloud(sam_img_path, depth_img_path, sam_json_path, intrinsic_path, depth_intrinsic_path, extrinsic_path, return_idx_mapping=False):
+def generate_sam_pointcloud(sam_img_path, depth_img_path, sam_json_path, intrinsic_path, depth_intrinsic_path, extrinsic_path, extra_extrinsic_path, return_idx_mapping=False):
     """
         Derives SAM point cloud from SAM image, depth image, SAM json, intrinsic, extrinsic.
         Args:
@@ -47,22 +47,28 @@ def generate_sam_pointcloud(sam_img_path, depth_img_path, sam_json_path, intrins
             intrinsic_path: path to intrinsic
             depth_intrinsic_path: path to depth intrinsic
             extrinsic_path: path to extrinsic
+            extra_extrinsic_path: path to extra extrinsic
+            return_idx_mapping: bool, whether to return the mapping from SAM ids to indices in group_of_points
         Returns:
             group_of_points: list of SAM point clouds, each point cloud is a numpy array of shape (N_i, 3)
             idx_mapping: (optional) dict, mapping from SAM ids to indices in group_of_points
     """
-    intrinsic = read_intrinsic(intrinsic_path)
+    intrinsic = read_intrinsic(intrinsic_path) # camera to image
     rgb_cx, rgb_cy, rgb_fx, rgb_fy = intrinsic[0][2], intrinsic[1][2], intrinsic[0][0], intrinsic[1][1]
-    depth_intrinsic = read_intrinsic(depth_intrinsic_path)
+    depth_intrinsic = read_intrinsic(depth_intrinsic_path) # camera to image
     depth_cx, depth_cy, depth_fx, depth_fy = depth_intrinsic[0][2], depth_intrinsic[1][2], depth_intrinsic[0][0], depth_intrinsic[1][1]
-    extrinsic = np.loadtxt(extrinsic_path)
-    depth = cv2.imread(depth_img_path, cv2.IMREAD_UNCHANGED)
+    extrinsic = np.loadtxt(extrinsic_path) # camera to world'
+    extra_extrinsic = np.load(extra_extrinsic_path) # world' to world
+    extrinsic = np.matmul(extra_extrinsic, extrinsic) # camera to world
+    # print("camera to world extrinsic:\n", extrinsic)
+    depth = cv2.imread(depth_img_path, cv2.IMREAD_UNCHANGED) # shape 480, 640
     assert depth is not None, "Failed to read depth image. Check path."
     depth = depth.astype(np.float32) / 1000.0
     with open(sam_json_path, 'r') as f:
         sam_id_json = json.load(f)
     sam_id = np.array(sam_id_json, dtype=int)
-    
+    print("sam_id.shape:{}".format(sam_id.shape)) # shape 480, 640
+
     group_id = np.unique(sam_id)
     group_of_points = []
     count = 0
@@ -73,12 +79,15 @@ def generate_sam_pointcloud(sam_img_path, depth_img_path, sam_json_path, intrins
             continue
         idx_mapping[id] = count
         points = []
-        us, vs = np.where(sam_id == id)
-        us = depth_cx + (us - rgb_cx) * depth_fx / rgb_fx
-        vs = depth_cy + (vs - rgb_cy) * depth_fy / rgb_fy
+        us, vs = np.where(sam_id == id) # rgb coordinates
+        us, vs = us.astype(float), vs.astype(float)
+        us = depth_cy + (us - rgb_cy) * depth_fy / rgb_fy # convert to depth coordinates
+        vs = depth_cx + (vs - rgb_cx) * depth_fx / rgb_fx # convert to depth coordinates
         us, vs = np.round(us).astype(int), np.round(vs).astype(int)
-        us = us[np.logical_and(us >= 0, us < depth.shape[1])]
-        vs = vs[np.logical_and(vs >= 0, vs < depth.shape[0])]
+        feasible_mask = np.logical_and(us >= 0, us < depth.shape[1])
+        feasible_mask = np.logical_and(feasible_mask, vs >= 0)
+        feasible_mask = np.logical_and(feasible_mask, vs < depth.shape[0])
+        us, vs = us[feasible_mask], vs[feasible_mask]
         ds = depth[us, vs]
         points = np.stack([vs * ds, us * ds, ds, np.ones((us.shape[0],))], axis=1) # shape (N, 4)
         points = points[ds > 0]
@@ -87,6 +96,7 @@ def generate_sam_pointcloud(sam_img_path, depth_img_path, sam_json_path, intrins
             continue
         xyzs = np.matmul(np.matmul(extrinsic, np.linalg.inv(depth_intrinsic)), points.transpose(1, 0)).transpose(1, 0)
         xyzs = xyzs[:,:3]
+        # print("minx:%.2f, maxx:%.2f, miny:%.2f, maxy:%.2f, minz:%.2f, maxz:%.2f" % (np.min(xyzs[:,0]), np.max(xyzs[:,0]), np.min(xyzs[:,1]), np.max(xyzs[:,1]), np.min(xyzs[:,2]), np.max(xyzs[:,2]))) # print the range o
         group_of_points.append(xyzs)
                 
         count += 1
@@ -112,6 +122,11 @@ def read_scannet_intrinsic(path):
 
 
 def read_intrinsic(path, mode='scannet'):
+    """
+        Reads intrinsic matrix from file.
+        Returns:
+            extended intrinsic of shape (4, 4)
+    """
     if mode =='scannet':
         return read_scannet_intrinsic(path)
     elif mode =='mp3d':
@@ -292,6 +307,7 @@ if __name__ == '__main__':
     intrinsic_path = "./example_data/posed_images/intrinsic.txt"
     depth_intrinsic_path = "./example_data/posed_images/depth_intrinsic.txt"
     extrinsic_path = "./example_data/posed_images/02300.txt"
+    extra_extrinsic_path = "./example_data/label/rot_matrix.npy"
     bbox_json_path = "./example_data/label/main_MDJH13.json"
-    sam_id_to_object_id = map_sam_id_to_bboxes(sam_img_path, depth_img_path, sam_json_path, intrinsic_path, depth_intrinsic_path, extrinsic_path, bbox_json_path)
+    sam_id_to_object_id = map_sam_id_to_bboxes(sam_img_path, depth_img_path, sam_json_path, intrinsic_path, depth_intrinsic_path, extrinsic_path, extra_extrinsic_path, bbox_json_path)
     print(sam_id_to_object_id)
