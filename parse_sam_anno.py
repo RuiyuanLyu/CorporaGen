b815d68a14f26f05dc35905c55db537cdc4ae047
RuiyuanLyu <ruiyuanlyu@gmail.com>
2024-01-17 20:07:20 +0800

    remove legacy files

--- parse_sam_anno.py
+++
@@ -1,9 +0,0 @@
-import json
-import os
-
-data_root = './example_data/'
-frame_id = '02300'
-
-sam_anno_file = os.path.join(data_root, 'sam',  frame_id + '.json')
-mask_anno_file = os.path.join(data_root, 'sam_2dmask', frame_id + '.json')
-
