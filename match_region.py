cb4c3f62071fa3e364a53e2db996e378ebb31daa
Eurake <101387791+rbler1234@users.noreply.github.com>
2024-02-04 00:35:18 +0800

    Add files via upload
    Notice: render.py is updated for the region segmentation ui!

--- match_region.py
+++ match_region.py
@@ -24,18 +24,20 @@ def is_in_poly(p, poly):
                 is_in = not is_in
     return is_in
 
-def get_position_in_render_image(point, ratio):
+def get_position_in_render_image(point,min_x, min_y,ratio=20):
     x, y= point
     pixel_x = int((x - min_x) * ratio)
     pixel_y = int((y - min_y) * ratio)
-    return pixel_y, pixel_x
+    return pixel_y,pixel_x
 
-def get_data(output_annotation_path, visibility_json_path, object_json_path, point_cloud_path):
+def get_data(input_annotation_path,point_cloud_path):
+    '''extract data from input files'''
+
+
     point_cloud = o3d.io.read_point_cloud(point_cloud_path)
 
-    region_with_label_file = open(output_annotation_path, "r", encoding="UTF-8")
-    visible_objects_file = open(visibility_json_path, "r")
-    object_json_file = open(object_json_path, "r")
+    region_with_label_file = open(input_annotation_path, "r", encoding="UTF-8")
+
     lines = region_with_label_file.readlines()
     s = ''
     for l in lines[0]:
@@ -45,15 +47,7 @@ def get_data(output_annotation_path, visibility_json_path, object_json_path, poi
             s += l
     region_with_label = json.loads(s)
 
-    content = visible_objects_file.read()
-    a = json.loads(content)
-    content = object_json_file.read()
-    object_data = json.loads(content)
-    visible_objects_id = []
-    for camera_id in a.keys():
-        for object_id in a[camera_id]:
-            if object_id not in visible_objects_id:
-                visible_objects_id.append(object_id)
+
 
     points = np.asarray(point_cloud.points)
     sorted_indices = np.argsort(points[:, 2])
@@ -62,20 +56,22 @@ def get_data(output_annotation_path, visibility_json_path, object_json_path, poi
     min_x, min_y, _ = np.min(points, axis=0)
     max_x, max_y, _ = np.max(points, axis=0)
 
-    return visible_objects_id, min_x, min_y, region_with_label, object_data
+    return min_x,min_y,region_with_label
 
-def process_data(region_with_label, object_data):
+def process_data(region_with_label,object_ids,bboxes,min_x, min_y):
+    '''process the annotation data'''
+
     for region in region_with_label:
 
         poly = region['vertex']
         region['object_ids']=[]
 
-        for object in object_data:
-            object_x = object["psr"]["position"]["x"]
-            object_y = object["psr"]["position"]["y"]
-            #print(get_position_in_render_image((object_x, object_y)))
-            if is_in_poly(get_position_in_render_image((object_x, object_y), ratio), poly) and int(object["obj_id"]) in visible_objects_id:
-                region['object_ids'].append(int(object["obj_id"]))
+        for _index in range(len(object_ids)):
+            object_x = bboxes[_index][0]
+            object_y = bboxes[_index][1]
+            #print(get_position_in_render_image((object_x,object_y)))
+            if is_in_poly(get_position_in_render_image((object_x,object_y),min_x, min_y),poly):
+                region['object_ids'].append(object_ids[_index])
     return region_with_label
 
 
@@ -89,46 +85,21 @@ if __name__ == "__main__":
 
     ratio = 20 #ratio of point to render image
 
-    visible_objects_id, min_x, min_y, region_with_label, object_data = get_data(input_annotation_path, visibility_json_path, object_json_path, point_cloud_path)
+    min_x, min_y, region_with_label = get_data(input_annotation_path,point_cloud_path)
 
-    region_with_label = process_data(region_with_label, object_data)
+    object_data = np.load("../CorporaGen-master2/CorporaGen-master/example.npy",allow_pickle=True).item()
+    bboxes = object_data['bboxes']
+    object_ids = object_data['object_ids']
 
-    #print(region_with_label)
+
+    region_with_label = process_data(region_with_label,object_ids,bboxes,min_x, min_y)
+
+
 
     os.makedirs(otuput_annotation_dir, exist_ok=True)
     with open(f"{otuput_annotation_dir}/{file_name}.txt", 'w') as file:
         file.write(str(region_with_label))
 
-    #render_image = cv2.imread("../example_data/point_cloud_top_view.png")
-    # print(shape_x, shape_y)
-    # print(render_image.shape)
-
-
-    # print(ratio)
-    # object_min_x=100
-    # object_min_y=100
-    # object_max_x=0
-    # object_max_y=0
-    # for object in b:
-    #     if int(object["obj_id"]) not in visible_objects_id:
-    #         continue
-    #     object_x = object["psr"]["position"]["x"]
-    #     object_y = object["psr"]["position"]["y"]
-    #     if object_x>object_max_x:
-    #         object_max_x = object_x
-    #     if object_y>object_max_y:
-    #         object_max_y = object_y
-    #     if object_x<object_min_x:
-    #         object_min_x = object_x
-    #     if object_y<object_min_y:
-    #         object_min_y = object_y
-    #     object_x, object_y=get_position_in_render_image((object_x, object_y), ratio)
-    #     size=1
-    #     render_image[max(object_x - size, 0):min(object_x + size, render_image.shape[0] - 1), max(object_y - size, 0):min(object_y + size, render_image.shape[1] - 1)] = np.array(
-    #         [255, 0, 0]).astype(np.uint8)
-    # cv2.imwrite('render_image.png', render_image)
-    # print(object_max_x, object_max_y)
-    # print(object_min_x, object_min_y)
 
 
 
