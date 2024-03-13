1abf7520d588191d65580773e0391a72d84fe093
RuiyuanLyu <ruiyuanlyu@gmail.com>
2024-03-13 13:21:42 +0800

    add core question: if the picture provides enough information for annotation.

--- object_text_ui.py
+++ object_text_ui.py
@@ -6,6 +6,7 @@ from utils_read import load_json
 
 QUESTIONS = {
     "meta": "这段文字是否在尝试描述框中的物体？",
+    "visual_info_sufficient": "选择的照片提供的信息是否足以对该物体进行描述？（信息不足的例子：遮挡太多/过于模糊）",
     "category": "物体类别是否准确？",
     "appearance": "物体的外观（形状颜色）是否准确？",
     "material": "物体的材质是否准确？",
@@ -25,7 +26,7 @@ def get_valid_directories():
     """
     valid_directories = []
     for dir_path, dir_names, file_names in os.walk("."):
-        if "corpora_object" in dir_names and ("painted_images" in dir_names or "painted_objects" in dir_names):
+        if any(name.startswith("corpora_object") for name in dir_names) and "painted_objects" in dir_names:
             valid_directories.append(dir_path)
     return valid_directories
 
@@ -35,15 +36,21 @@ with gr.Blocks() as demo:
         directory = gr.Dropdown(label="Select a directory", choices=valid_directories)
         object_name = gr.Dropdown(label="Select an object", value="Select an object", allow_custom_value=True, interactive=True)
 
-    original_description = gr.Textbox(label="Original Description", interactive=False)
+    with gr.Row():
+        original_description = gr.Textbox(label="Original Description", interactive=False)
+        backup_description = gr.Textbox(label="Backup Description", value="", visible=False, interactive=False)
     warning_text = gr.Textbox(label="Warning", value="", visible=False, interactive=False)
-    translated_description = gr.Textbox(label="Translated Description", interactive=False)
-
-    core_question = gr.Radio(label="Question 0", choices=["是", "否", "不要选这个选项"], value="不要选这个选项", info=QUESTIONS["meta"], interactive=True)
+    with gr.Row():
+        translated_description = gr.Textbox(label="Translated Description", interactive=False)
+        backup_translated_description = gr.Textbox(label="Backup Translated Description", value="", visible=False, interactive=False)
+    
+    with gr.Row():
+        core_question = gr.Radio(label="Core Question", choices=["是", "否", "不要选这个选项"], value="不要选这个选项", info=QUESTIONS["meta"], interactive=True)
+        core_question2 = gr.Radio(label="Core Question 2", choices=["是", "否", "不要选这个选项"], value="不要选这个选项", info=QUESTIONS["visual_info_sufficient"], interactive=True)
     with gr.Row():
         questions_radio = []
         # Questions for the user to answer
-        num_questions = 9
+        num_questions = len(KEYS)
         questions = [f"Q{i}" for i in range(num_questions)]
         choices = ["不要选这个选项"]
         for i in range(num_questions):
@@ -54,6 +61,7 @@ with gr.Blocks() as demo:
     with gr.Row():
         with gr.Column():
             image = gr.Image(label="Image", type="filepath")
+            aux_image = gr.Image(label="Auxiliary Image", type="filepath")
         with gr.Column():
             textboxes = []
             for i in range(max_textboxes):
@@ -73,20 +81,21 @@ with gr.Blocks() as demo:
                 is_valid, error_message = check_annotation_validity(annotation)
                 if is_valid:
                     object_name = json_file.split(".")[0]
-                    object_name = "_".join(object_name.split("_")[:3]) # object name can be like: 001_table_05100 or 001_table_05100_cogvlm. We only need the first 3 parts.
                     valid_objects.append(object_name)
         return gr.Dropdown(label="Select an object", choices=valid_objects, allow_custom_value=True)
     directory.change(fn=update_object_name_choices, inputs=directory, outputs=[object_name])
 
-    def get_description_and_image_path(object_name, directory):
+    def get_description(json_file_path, is_aux=False):
         """
-        Returns the description and image path of the given object.
+            Args:
+                json_file_path: str, the path to the JSON file.
+                is_aux: bool, whether the JSON file is an auxiliary file or not.
+            Returns:
+                original_description: str, the original description of the object.
+                translated_description: str, the translated description of the object.
+                warning_text: str or None, the warning message if the translated description is not available.
         """
-        json_file = os.path.join(directory, "corpora_object", f"{object_name}.json")
-        if not os.path.exists(json_file):
-            json_file = os.path.join(directory, "corpora_object", f"{object_name}_cogvlm.json")
-        annotation = load_json(json_file)
-        warning_text = gr.Textbox(label="Warning", value="", visible=False, interactive=False)
+        annotation = load_json(json_file_path)
         assert isinstance(annotation, dict), f"Invalid annotation type: {type(annotation)}"
         if "simplified_description" in annotation:
             original_description = annotation["simplified_description"]
@@ -94,20 +103,42 @@ with gr.Blocks() as demo:
             original_description = annotation["original_description"]
         if "modified_description" in annotation:
             translated_description = annotation["modified_description"]
-            warning_text = gr.Textbox(label="Warning", value="该物体的描述此前已经被修改过。", visible=True, interactive=False)
+            warning_text = "提示：该物体的描述此前已经被修改过。"
+            if not is_aux:
+                gr.Info("提示：该物体的描述此前已经被修改过。")
         else:
-            translated_description = annotation.get("translated_description", '没有可用的中文描述.请在Q0选择"否"。')
-        image_path = os.path.join(directory, "painted_images", f"{object_name}.jpg")
-        return original_description, translated_description, image_path, warning_text
-    object_name.change(fn=get_description_and_image_path, inputs=[object_name, directory], outputs=[original_description, translated_description, image, warning_text])
+            translated_description = annotation.get("translated_description", '没有可用的中文描述。如果左右都没有，请在Q0中选择“否”。')
+            warning_text = None
+        return original_description, translated_description, warning_text
 
-    def refresh_core_question():
-        core_question = gr.Radio(label="Question 0", choices=["是", "否", "不要选这个选项"], value="不要选这个选项", info=QUESTIONS["meta"], interactive=True)
-        return core_question
-    object_name.change(fn=refresh_core_question, inputs=None, outputs=core_question)
+    def get_description_and_image_path(object_name, directory):
+        """
+        Returns the description and image path of the given object.
+        """
+        json_file = os.path.join(directory, "corpora_object", f"{object_name}.json")
+        original_description, translated_description, warning_text = get_description(json_file)
+        backup_json_file = os.path.join(directory, "corpora_object_gpt4v", f"{object_name}.json")
+        if os.path.exists(backup_json_file):
+            backup_description, backup_translated_description, _ = get_description(backup_json_file, is_aux=True)
+            backup_description = gr.Textbox(label="Backup Description", value=backup_description, visible=True, interactive=False)
+            backup_translated_description = gr.Textbox(label="Backup Translated Description", value=backup_translated_description, visible=True, interactive=False)
+        else:
+            backup_description = gr.Textbox(label="Backup Description", value="", visible=False, interactive=False)
+            backup_translated_description = gr.Textbox(label="Backup Translated Description", value="", visible=False, interactive=False)
+        warning_text = gr.Textbox(label="Warning", value=warning_text, visible=bool(warning_text), interactive=False)
+        image_path = os.path.join(directory, "painted_objects", f"{object_name}.jpg")
+        image_path2 = os.path.join(directory, "cropped_objects", f"{object_name}.jpg")
+        return original_description, translated_description, backup_description, backup_translated_description, image_path, image_path2, warning_text
+    object_name.change(fn=get_description_and_image_path, inputs=[object_name, directory], outputs=[original_description, translated_description, backup_description, backup_translated_description, image, aux_image, warning_text])
 
-    def refresh_questions(core_question):
-        visible = map_choice_to_bool(core_question)
+    def refresh_core_questions():
+        core_question = "不要选这个选项"
+        core_question2 = "不要选这个选项"
+        return core_question, core_question2
+    object_name.change(fn=refresh_core_questions, inputs=None, outputs=[core_question, core_question2])
+
+    def refresh_questions(core_question, core_question2):
+        visible = map_choice_to_bool(core_question) and map_choice_to_bool(core_question2)
         questions_radio = []
         # Questions for the user to answer
         questions = [QUESTIONS.get(k, "") for k in KEYS]
@@ -116,8 +147,9 @@ with gr.Blocks() as demo:
             r = gr.Radio(label=f"Question {i+1}", choices=choices, value=choices[-1], info=questions[i], interactive=True, visible=visible)
             questions_radio.append(r)
         return questions_radio
-    object_name.change(fn=refresh_questions, inputs=core_question, outputs=questions_radio)
-    core_question.change(fn=refresh_questions, inputs=core_question, outputs=questions_radio)
+    object_name.change(fn=refresh_questions, inputs=[core_question, core_question2], outputs=questions_radio)
+    core_question.change(fn=refresh_questions, inputs=[core_question, core_question2], outputs=questions_radio)
+    core_question2.change(fn=refresh_questions, inputs=[core_question, core_question2], outputs=questions_radio)
 
     def refresh_save_button(core_question):
         visible = core_question in ["是", "否"]
@@ -157,7 +189,7 @@ with gr.Blocks() as demo:
         t.change(fn=update_preview_description, inputs=[*textboxes], outputs=preview_description)
     save_button.click(fn=update_preview_description, inputs=[*textboxes], outputs=preview_description)
 
-    def save_annotations(directory, object_name, core_question, preview_description, *questions_radio):
+    def save_annotations(directory, object_name, core_question, core_question2, preview_description, *questions_radio):
         """
         Saves the annotations to the given directory.
         """
@@ -170,7 +202,8 @@ with gr.Blocks() as demo:
 
         accuracy_dict = {} # whether the attribute (key) of the original description is accurate or not
         accuracy_dict["meta"] = map_choice_to_bool(core_question)
-        if map_choice_to_bool(core_question):
+        accuracy_dict["visual_info_sufficient"] = map_choice_to_bool(core_question2)
+        if map_choice_to_bool(core_question) and map_choice_to_bool(core_question2):
             for i in range(len(questions_radio)):
                 key = KEYS[i]
                 value = map_choice_to_text(questions_radio[i])
@@ -188,8 +221,8 @@ with gr.Blocks() as demo:
             json.dump(annotation_to_save, f, ensure_ascii=False, indent=4)
         save_button = gr.Button(value="保存成功！请选择下一物体。")
         return save_button
-    save_button.click(fn=save_annotations, inputs=[directory, object_name, core_question, preview_description, *questions_radio], outputs=save_button)
-
+    save_button.click(fn=save_annotations, inputs=[directory, object_name, core_question, core_question2, preview_description, *questions_radio], outputs=save_button)
+demo.queue(concurrency_count=20)
 
 if __name__ == "__main__":
     demo.launch()
