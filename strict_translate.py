675f228512aabf8f776a30e405fafe7116d42ea5
RuiyuanLyu <ruiyuanlyu@gmail.com>
2024-05-06 23:33:11 +0800

    resolve for too long lists

--- strict_translate.py
+++ strict_translate.py
@@ -4,8 +4,7 @@ from tqdm import tqdm
 from utils.openai_api import mimic_chat, mimic_chat_budget, get_content_groups_from_source_groups, num_tokens_from_string, get_full_response
 from utils.utils_read import load_json
 
-@DeprecationWarning
-def list_translate(text, src_lang="English", tgt_lang="Chinese"):
+def list_translate(texts, src_lang="English", tgt_lang="Chinese"):
     """
         Translates a list of text using the OpenAI API.
         Args:
@@ -15,10 +14,10 @@ def list_translate(text, src_lang="English", tgt_lang="Chinese"):
         Returns:
             A list of translated text strings.
     """
-    user_message = text
+    user_message = str(texts)
     src_lang = src_lang.capitalize()
     tgt_lang = tgt_lang.capitalize()
-    system_prompt = f'You are an excellent translator.I will give you a list of sentences, you are supposed to translate every sentence in the list from {src_lang} to {tgt_lang}.The result should be only a list(Use double quotation marks as string delimiters, like this:["here is a example","you are happy"]) , just only give me the list in your answer.'
+    system_prompt = f'You are an excellent translator.I will give you a list of sentences, you are supposed to translate every sentence in the list from {src_lang} to {tgt_lang}. The result should be only a list (Use double quotation marks as string delimiters, like this:["here is a example","you are happy"]). Repeat: provide a list in the answer.'
     source_groups = [
         [user_message],
     ]
@@ -27,16 +26,42 @@ def list_translate(text, src_lang="English", tgt_lang="Chinese"):
     for message in conversation:
         if message["role"] == "assistant":
             return message["content"]
-def strict_list_translate(texts,src_lang="English", tgt_lang="Chinese",max_try=10):
-    '''
-        here I want to make sure the translate
-    '''
+        
+def disassemble_parts(texts:list[str], max_token_length=600, max_list_length=10):
+    """
+        disassembles the text into parts that are smaller than the maximum token length.
+        Returns:
+            A list of parts.
+    """
+    parts = []
+    current_part = []
+    current_token_length = 0
+    last_i = 0
+    for i, text in enumerate(texts):
+        if current_token_length+num_tokens_from_string(text)>max_token_length or i-last_i>max_list_length:
+            parts.append(current_part)
+            current_part = []
+            current_token_length = 0
+            last_i = i
+        current_part.append(text)
+        current_token_length += num_tokens_from_string(text)
+    if current_part:
+        parts.append(current_part)
+    return parts
+
+def strict_list_translate_part(texts,src_lang="English", tgt_lang="Chinese",max_try=10):
+    """
+        Translates a list of text using the OpenAI API.
+        Strictly checks the output to make sure the output is a list of strings.
+        Returns:
+            A list of translated text strings, num tries, and total token usage.
+    """
     success = False
     _try = 0
     while not success and _try<max_try:
         _try+=1
         success = True
-        out = list_translate(str(texts),src_lang, tgt_lang)
+        out = list_translate(texts,src_lang, tgt_lang)
         a=b=-1
         for _index in range(len(out)):
             if out[_index]=='[':
@@ -60,8 +85,17 @@ def strict_list_translate(texts,src_lang="English", tgt_lang="Chinese",max_try=1
         return texts_out,_try
     else:
         return [],_try
-
-
+        
+def strict_list_translate(texts:list[str],src_lang="English", tgt_lang="Chinese",max_try=10):
+    parts = disassemble_parts(texts)
+    out_list = []
+    max_num_tries = 0
+    total_token_usage = {}
+    for part in parts:
+        part_translated, num_tries = strict_list_translate_part(part, src_lang, tgt_lang, max_try)
+        out_list.extend(part_translated)
+        max_num_tries = max(max_num_tries, num_tries)
+    return out_list, max_num_tries
 
 def strict_check(text):
     angle_list = []
--- structure_translate.py
+++ structure_translate.py
@@ -1,30 +1,55 @@
-from strict_translate import strict_list_translate
 import numpy as np
-
+import logging
+import os
 from object_text_anno import mmengine_track_func
+from strict_translate import strict_list_translate
+import re
+
+def is_chinese(text):
+    # 正则表达式匹配中文字符
+    pattern = re.compile(r'[\u4e00-\u9fff]')
+    if pattern.search(text):
+        return True
+    else:
+        return False
 
 @mmengine_track_func
 def anno_translation(region_info_file,src_lang,tgt_lang,need_translate_index = [1,3,4,5]):
+    save_file = region_info_file.replace('.npy','_trans.npy')
     region_info = np.load(region_info_file,allow_pickle=True)
+    if not region_info.any():
+        return region_info
+    if os.path.exists(save_file):
+        return np.load(save_file,allow_pickle=True)
     total_list = []
     for _index in need_translate_index:
-        total_list+= [region_info[_index][k] for k in region_info[_index].keys()]
-
-    output_list,try_ = strict_list_translate(total_list,src_lang,tgt_lang)
-    print(try_)
-
+        total_list += [region_info[_index][k] for k in region_info[_index].keys()]
+    # import pdb; pdb.set_trace()
+    output_list, num_trys = strict_list_translate(total_list, src_lang, tgt_lang)
+    # logging.warning(f"Translated with {num_trys} trys, prompt_tokens: {token_usage['prompt_tokens']}, completion_tokens: {token_usage['completion_tokens']}")
+    logging.warning(f"Translated with {num_trys} trys for {region_info_file}")
+    if num_trys >= 10:
+        logging.warning(f"Failed to translate {region_info_file}")
+        return region_info
     start = 0
     for _index in need_translate_index:
-        for k in range(len(region_info[_index].keys())):
-            _key = list(region_info[_index].keys())[k]
-            region_info[_index][_key] = output_list[k+start]
-        start+=len(region_info[_index].keys())
-    np.save(region_info_file.replace('.npy','_trans.npy'),region_info)
+        for key in region_info[_index].keys():
+            region_info[_index][key] = output_list[start]
+            start+=1
+    all_chinese = [is_chinese(text) for text in output_list]
+    if not all(all_chinese):
+        logging.warning(f"Warning: NOT all translated texts are chinese for {region_info_file}")
+    np.save(save_file,region_info)
     return region_info
 
 if __name__ == "__main__":
     DATA_ROOT = 'data'
     REGION_VIEW_DIR_NAME = 'region_views'
+    log_file_name = f"{__file__[:-3]}.log"
+    print(f"log will be written to {log_file_name}")
+    # Set level to warning to avoid writing HTTP requests to log
+    logging.basicConfig(filename=log_file_name, level=logging.WARNING, format='%(asctime)s - %(message)s')
+
     # scene_id = 'scene0000_00'
     # data_dir = f'data/{scene_id}/region_views/4_toliet region'
     # region_info_file = os.path.join(data_dir,'struction.npy')
@@ -35,7 +60,7 @@ if __name__ == "__main__":
     # np.save(data_dir+'/struction_trans.npy',out)
     import os
     scene_ids = os.listdir(DATA_ROOT)
-    scene_ids = [scene_id for scene_id in scene_ids if scene_id.startswith('scene') or scene_id.startswith('1mp3d')]
+    scene_ids = [scene_id for scene_id in scene_ids if scene_id.startswith('scene') or scene_id.startswith('1mp3d') or scene_id.startswith('3rscan')]
     scene_ids = sorted(scene_ids)
     tasks = []
     for scene_id in scene_ids:
@@ -52,6 +77,7 @@ if __name__ == "__main__":
                 continue
             tasks.append((region_info_file,"English","Chinese"))
 
+    anno_translation(tasks[0])
     import mmengine
     mmengine.utils.track_parallel_progress(anno_translation, tasks, nproc=8)
 
